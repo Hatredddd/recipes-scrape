95432b92f60c675a1b00d8419214df0fef4d357d
EgorYorsh <118363702+Hatredddd@users.noreply.github.com>
2023-02-17 20:13:11 +0300

    Delete QR directory

--- QR/main.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-
-def get_qrcode(url='https://cv2you.com/resume/SehY/',name='MyQR'):
-    qr=qrcode.make(data=url)
-    qr.save(stream=f'{name}.png')
-    return f'QR code was created! Open the {name}.png'
-
-if __name__=='__main__':
-    get_qrcode()
